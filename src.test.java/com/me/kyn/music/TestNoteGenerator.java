6785a332437f4442da04eed1d156178c55ad1b97
soundobj <soundobj@yahoo.com>
2013-12-31 19:09:01 +0000

    commit n push test

--- src.test.java/com/me/kyn/music/TestNoteGenerator.java
+++ src.test.java/com/me/kyn/music/TestNoteGenerator.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class TestNoteGenerator extends TestCase {
 	NoteGenerator noteGenerator;
+	//commit push test
 	List<Note> notes;	
 	@Test
 	public void testgetNotes () {
